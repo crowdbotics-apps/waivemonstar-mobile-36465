9fc244f8df53a530544581006eb8a0d2908525f8
Crowdbotics <team@crowdbotics.com>
2022-07-28 14:14:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "waivemonstar_mobile_36465",
+  url: "https://waivemonstar_mobile_36465.botics.co",
+  api: "https://waivemonstar_mobile_36465.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
